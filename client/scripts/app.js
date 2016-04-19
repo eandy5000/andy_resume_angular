1d56bb2507ea22c0e2714a99ff335aa09c65e3bf
E. Andrew Eccleston <eandreweccleston@Es-MacBook-Pro.local>
2016-04-19 09:40:26 -0500

    project working

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -9,6 +9,18 @@
     
     app.controller('ProjectsCtrl', ['$scope',function($scope){
         $scope.tab = 1;
+        
+        $scope.setTab = function (num) {
+            return $scope.tab = num;
+        }
+        
+        $scope.checkTab = function (num){
+            if ($scope.tab === num) {
+                return true;
+            }   else {
+                return false;
+            }
+        }
     }]);
     
     
